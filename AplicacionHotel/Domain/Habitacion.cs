0fad649e8bb02583d49aa8a5e45e0aec15b32603
Milton <Milton@DESKTOP-P6G4O5Q>
2017-08-16 19:15:58 -0300

    clase Habitacion creada

---
+++ AplicacionHotel/Domain/Habitacion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionHotel.Domain
+{
+    class Habitacion
+    {
+        //ATRIBUTOS
+        private int iNro;
+        private int iCapacidad;
+        private int iPlanta;
+        private bool iExclusiva; //bool o boolean usar?
+
+        //CONSTRUCTOR
+        public Habitacion(int pNro, int pCapacidad, int pPlanta, bool pExclusiva)
+        {
+            this.iNro = pNro;
+            this.iCapacidad = pCapacidad;
+            this.iPlanta = pPlanta;
+            this.iExclusiva = pExclusiva;
+        }
+
+        //PROPIEDADES
+        public int Nro
+        {
+            get { return this.iNro; }
+        }
+        public int Capacidad
+        {
+            get { return this.iCapacidad; }
+        }
+        public int Planta
+        {
+            get { return this.iPlanta; }
+        }
+        public bool Exclusiva
+        {
+            get { return this.iExclusiva; }
+        }
+
+        //METODOS
+    }
+}
